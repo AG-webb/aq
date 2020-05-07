83aecadd23b0e1dc29e7ce1cdadb22492a568681
AG-webb <arikgevorgyan@mail.ru>
2020-05-07 15:11:48 +0400

    new

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
 let burger = document.querySelector('.nav-burger');
 let menu = document.querySelector('.mobile-menu');
+let lang = document.querySelector('.lang');
+let body = document.body;
+
+lang.addEventListener('click', function() {
+    togle(this, 'active');
+});
 
 burger.addEventListener('click', function() {
-    this.classList.toggle('active');
-    menu.classList.toggle('active');
-    document.body.classList.toggle('lock');
+    togle(this, 'active');
+    togle(menu, 'active');
+    togle(body, 'lock');
 });
+
+
+let x = window.matchMedia("(min-width: 992px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction);
+
+
+$(window).scroll(function() {
+    headerFixed();  
+});
+
+
+// Functions
+function headerFixed(){
+    var main = $( "main" );
+    var offset_main = main.offset();
+
+    if (offset_main.top<=$(window).scrollTop()) {
+        $(".nav-bar").addClass("fixed");
+    } else {
+        $(".nav-bar").removeClass("fixed");
+    }
+}
 
 
 // if window width is more than 992px hide mobile menu
@@ -17,6 +46,6 @@ function myFunction(x) {
     }
 }
 
-let x = window.matchMedia("(min-width: 992px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction);
+function togle(element, add) {
+    return element.classList.toggle(add);
+} 
