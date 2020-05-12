c1042050053d7e4c82b1589cc41726eb93522579
AG-webb <arikgevorgyan@mail.ru>
2020-05-12 17:25:08 +0400

    verjnakan

--- js/map.js
+++ js/map.js
@@ -1,14 +1,16 @@
 let lat = "51.505", lng = " -0.09";
-  lat = parseFloat(lat);
-  lng = parseFloat(lng);
-  // initialize the map
-  let mymap = L.map('map').setView([lat, lng], 14);
-  let myFilter = ['bright:100%','saturate:0%'];
-  L.tileLayer.colorFilter(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '',
-      filter: myFilter
-    }
-  ).addTo(mymap);
+lat = parseFloat(lat);
+lng = parseFloat(lng);
+
+// initialize the map
+let mymap = L.map('map').setView([lat, lng], 14);
+let myFilter = ['grayscale:100%','bright:110%',,'contrast:120%'];
+
+L.tileLayer.colorFilter(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '',
+    filter: myFilter
+}
+).addTo(mymap);
   
 let marker = L.marker([lat, lng]).addTo(mymap);
