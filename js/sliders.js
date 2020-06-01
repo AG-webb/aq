3b20475663859b68b1aebee040e9982d53859205
AG-webb <arikgevorgyan@mail.ru>
2020-06-01 14:37:01 +0400

    new

--- js/sliders.js
+++ js/sliders.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
     $('.slider-img').slick({
         dots: false,
         speed: 1000,
-        slidesToShow: 2,
-        autoplay: true,
-        autoplaySpeed: 5000,
+        slidesToShow: 1,
+        // autoplay: true,
+        // autoplaySpeed: 5000,
         asNavFor: '.slider-text',
     });
 
     $('.slider-text').slick({
         slidesToShow: 1,
-        autoplay: true,
+        // autoplay: true,
         speed: 1000,
-        autoplaySpeed: 5000,
+        // autoplaySpeed: 5000,
         arrows: false,
         adaptiveHeight: true,
         asNavFor: '.slider-img',
